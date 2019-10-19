e9738690d66dbe7f72754eeb4d52165ebc22a27e
Zoltán Szabó <zoltan.szabo@atolldev.hu>
2019-10-19 11:25:18 +0200

    Init Spring Boot Application

---
+++ src/main/java/com/szabodev/example/security/SpringAngularSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.szabodev.example.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAngularSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/szabodev/example/security/SpringAngularSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szabodev.example.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
